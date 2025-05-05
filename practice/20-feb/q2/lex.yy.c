c8797531ed4f4203e819ba8b304e73dd0f8ac026
shaswatag <gshaswata1@gmail.com>
2025-05-05 16:53:13 +0000

    fix(practice): 20-feb/q2 handle words starting with consonants

--- practice/20-feb/q2/lex.yy.c
+++ practice/20-feb/q2/lex.yy.c
@@ -351,8 +351,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 4
-#define YY_END_OF_BUFFER 5
+#define YY_NUM_RULES 5
+#define YY_END_OF_BUFFER 6
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -360,27 +360,28 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[11] =
+static const flex_int16_t yy_accept[13] =
     {   0,
-        0,    0,    5,    2,    2,    1,    2,    3,    1,    0
+        0,    0,    6,    4,    3,    3,    1,    2,    3,    1,
+        2,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    2,    2,
+        1,    1,    1,    1,    1,    1,    1,    1,    2,    3,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    2,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    3,    4,    4,    4,    3,    4,
-        4,    4,    3,    4,    4,    4,    4,    4,    3,    4,
-        4,    4,    4,    4,    3,    4,    4,    4,    4,    4,
-        1,    1,    1,    1,    1,    1,    3,    4,    4,    4,
+        1,    1,    1,    1,    4,    5,    5,    5,    4,    5,
+        5,    5,    4,    5,    5,    5,    5,    5,    4,    5,
+        5,    5,    5,    5,    4,    5,    5,    5,    5,    5,
+        1,    1,    1,    1,    1,    1,    4,    5,    5,    5,
 
-        3,    4,    4,    4,    3,    4,    4,    4,    4,    4,
-        3,    4,    4,    4,    4,    4,    3,    4,    4,    4,
-        4,    4,    1,    1,    1,    1,    1,    1,    1,    1,
+        4,    5,    5,    5,    4,    5,    5,    5,    5,    5,
+        4,    5,    5,    5,    5,    5,    4,    5,    5,    5,
+        5,    5,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -397,33 +398,33 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[5] =
+static const YY_CHAR yy_meta[6] =
     {   0,
-        1,    2,    3,    3
+        1,    2,    2,    3,    3
     } ;
 
-static const flex_int16_t yy_base[14] =
+static const flex_int16_t yy_base[16] =
     {   0,
-        0,    0,    9,    0,    6,    0,    0,    0,    0,   10,
-        4,    3,    3
+        0,    0,    9,   10,    0,    0,    0,    0,    0,    0,
+        0,   10,    6,    4,    3
     } ;
 
-static const flex_int16_t yy_def[14] =
+static const flex_int16_t yy_def[16] =
     {   0,
-       10,    1,   10,   11,   11,   12,   11,   13,   12,    0,
-       10,   10,   10
+       12,    1,   12,   12,   13,   13,   14,   15,   13,   14,
+       15,    0,   12,   12,   12
     } ;
 
-static const flex_int16_t yy_nxt[15] =
+static const flex_int16_t yy_nxt[16] =
     {   0,
-        4,    5,    6,    4,    8,    9,    7,    8,   10,    3,
-       10,   10,   10,   10
+        4,    5,    6,    7,    8,   11,   10,    9,   12,    3,
+       12,   12,   12,   12,   12
     } ;
 
-static const flex_int16_t yy_chk[15] =
+static const flex_int16_t yy_chk[16] =
     {   0,
-        1,    1,    1,    1,   13,   12,   11,    5,    3,   10,
-       10,   10,   10,   10
+        1,    1,    1,    1,    1,   15,   14,   13,    3,   12,
+       12,   12,   12,   12,   12
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -446,8 +447,8 @@ Find whether a letter is vowel or not
 */
 #line 6 "test.l"
 #include<stdio.h>
-#line 450 "lex.yy.c"
 #line 451 "lex.yy.c"
+#line 452 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -666,7 +667,7 @@ YY_DECL
 	{
 #line 9 "test.l"
 
-#line 670 "lex.yy.c"
+#line 671 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -693,7 +694,7 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 11 )
+				if ( yy_current_state >= 13 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -732,7 +733,6 @@ YY_RULE_SETUP
 }
 	YY_BREAK
 case 2:
-/* rule 2 can match eol */
 YY_RULE_SETUP
 #line 15 "test.l"
 {
@@ -748,10 +748,15 @@ YY_RULE_SETUP
 	YY_BREAK
 case 4:
 YY_RULE_SETUP
-#line 20 "test.l"
+#line 21 "test.l"
+{ printf("\nUnknown character: %s\n", yytext); return 1;}
+	YY_BREAK
+case 5:
+YY_RULE_SETUP
+#line 23 "test.l"
 ECHO;
 	YY_BREAK
-#line 755 "lex.yy.c"
+#line 760 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1048,7 +1053,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 11 )
+			if ( yy_current_state >= 13 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1076,11 +1081,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 11 )
+		if ( yy_current_state >= 13 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 10);
+	yy_is_jam = (yy_current_state == 12);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1756,12 +1761,13 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 20 "test.l"
+#line 23 "test.l"
 
 
 int main() {
     printf("\nEnter a string: ");
     yylex();
+    return 0;
 }
 
 int yywrap() {
